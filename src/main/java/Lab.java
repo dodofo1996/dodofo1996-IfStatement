7360c90866812fb8a9047365474a645d97e7b741
dodofo1996 <dodofo1996@gmail.com>
2023-02-17 03:37:02 +0000

    if stat with method

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import kotlin.contracts.Returns;
 
 public class Lab {
     /**
@@ -20,6 +21,17 @@ public class Lab {
      * @return x if bool is true. if bool is false, return y.
      */
     public int ifExample(boolean bool, int x, int y){
-        return 0;
+
+        
+
+        if (  bool == true ){
+            return x;
+         
+
+        }else {
+                return y;
+             }
     }
 }
+
+
